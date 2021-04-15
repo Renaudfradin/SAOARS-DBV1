6984556d98d4a48d75d51f557c81b5ae0aa4cf4f
Renaudfradin <renaudfradin@gmail.com>
2021-04-16 01:04:15 +0200

    seach bar

--- src/components/Personages/Personages.vue
+++ src/components/Personages/Personages.vue
@@ -3,10 +3,20 @@
         <div class="main_content">
             <div class="info">
                 <h1>personages</h1>
-                <button v-on:click.prevent="callapi">test api</button>
-                <ul idss="persoss.name">
-                    <li v-for="persoss in persos" :key="persoss.id"><router-link :to="{ name: 'personageid' , params: { id:persoss.id }}">{{persoss.name}}</router-link> {{persoss.description}} {{persoss.typeattack1}} {{persoss.typeattack2}} {{persoss.typeattack3}}</li>
-                </ul>
+                <div>
+                    <input type="text" @keyup="searchperso" v-model="queryperso" placeholder="rechecher un perso">
+                </div>
+                <!-- <button v-on:click.prevent="callapi">test api</button> -->
+                <div v-if="perso.length == 0">
+                    <ul idss="persoss.name" v-for="persoss in persos" :key="persoss.id">
+                        <li><router-link :to="{ name: 'personageid' , params: { id:persoss.id }}">{{persoss.name}}</router-link> {{persoss.description}} {{persoss.typeattack1}} {{persoss.typeattack2}} {{persoss.typeattack3}}</li>
+                    </ul>
+                </div> 
+                <div v-else>
+                    <ul idss="persoss.name" v-for="persoss in perso" :key="persoss.id">
+                        <li><router-link :to="{ name: 'personageid' , params: { id:persoss.id }}">{{persoss.name}}</router-link> {{persoss.description}} {{persoss.typeattack1}} {{persoss.typeattack2}} {{persoss.typeattack3}}</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -17,19 +27,30 @@ export default {
     name: 'Personages',
     data(){
         return {
-            perso: undefined,
+            perso: [],
             persos: [],
-            urlapi:'https://api-saoars.herokuapp.com/perso'
+            urlapi:'https://api-saoars.herokuapp.com/perso',
+            queryperso:'',
+
         }
     },
     methods:{
-        callapi:function() {
+       /* callapi:function() {
             axios
             .get(`${this.urlapi}/45`)
             .then(reponse => {    
                 this.perso = reponse.data.Personage; 
                 console.log(this.perso);
             })
+        }*/
+        searchperso(){
+            axios
+            .get(`${this.urlapi}/p/${this.queryperso}`)
+            .then(reponse => {
+                this.perso = reponse.data.Personage;
+                console.log(this.perso);
+            })
+            .catch(this.persos)
         }
     },
     mounted(){
