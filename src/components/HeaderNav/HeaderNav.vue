6499ec4ab47dd1022b573104eec97bb6d0e26be1
Renaudfradin <renaudfradin@gmail.com>
2021-04-29 23:10:45 +0200

    nav menu responsive

--- src/components/HeaderNav/HeaderNav.vue
+++ src/components/HeaderNav/HeaderNav.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="wrapper">
-        <div class="sidebar">
-            <router-link to="/"><h2>SAOARS DB</h2></router-link>
+        <div class="navhori" v-bind:class="{navhorinone: nenuetrue}">
+            <div class="navhori_div">
+                <div>
+                    <button v-on:click="nenuetrue =!nenuetrue" class="btn_menu">
+                        <div v-if="nenuetrue == true"><img class="img_menu" src="../../assets/navigation.png" alt=""></div>
+                        <div v-else><img class="img_menu" src="../../assets/cancel1.png" alt=""></div>
+                    </button>
+                </div>
+                <div class="navhori_title">
+                    <router-link to="/"><h2>SAOARS DB</h2></router-link>
+                </div>
+            </div>
+        </div>
+        <div v-bind:class="{sidebar, none : nenuetrue}">
             <ul>
                 <li><router-link to="/" class="home_link"><img class="sidebar_img" src="../../assets/nav_main.png" alt=""><p class="sidebar_p">Home</p></router-link></li>
                 <li><router-link to="/personages"><img class="sidebar_img" src="../../assets/nav_character.png" alt=""><p class="sidebar_p">Personages</p></router-link></li>
@@ -15,7 +27,16 @@
 </template>
 <script>
 export default {
-    name:'HeaderNav'
+    name:'HeaderNav',
+    data(){
+        return{
+            nenuetrue: true,
+            sidebar: true
+        }
+    }, 
+    methods: {
+        
+    }
 }
 </script>
 <style src="./HeaderNav.css" scoped></style>
